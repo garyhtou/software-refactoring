058c201269647122640db5df361fba91890b931a
Gary Tou <gary@garytou.com>
2023-05-31 00:05:29 -0700

    Extract method (with parameterized method)
    The logic for opening, closing, and saving a file as been extracted out.
    But, this wraps around the logic for writing the file content, so the
    extracted method takes in a lambda which handles writing the file
    contents (aka parameterized method).

--- hw3/q1/refactored.cpp
+++ hw3/q1/refactored.cpp
@@ -1,12 +1,11 @@
-// Generate the .pxd file for the library.  This will be used when creating
-// the Python module
-static void makePXDFile(std::vector<FnSymbol*> functions) {
-  fileinfo pxd = { NULL, NULL, NULL };
+static void makePythonFile(const char* fileName, std::string fileExtension,
+                           const std::function<void(fileinfo&)>& writeFileContents) {
+  fileinfo file = { NULL, NULL, NULL };
 
-  openLibraryHelperFile(&pxd, pxdName.c_str(), "pxd");
+  openLibraryHelperFile(&file, fileName, fileExtension);
 
-  if (pxd.fptr == NULL) {
-    closeLibraryHelperFile(&pxd, false);
+  if (file.fptr == NULL) {
+    closeLibraryHelperFile(&file, false);
     return;
   }
 
@@ -14,17 +13,7 @@ static void makePXDFile(std::vector<FnSymbol*> functions) {
 
   gGenInfo->cfile = pxd.fptr;
 
-  fprintf(pxd.fptr, "from libc.stdint cimport *\n");
-  // Get the permanent runtime definitions
-  fprintf(pxd.fptr, "from chplrt cimport *\n\n");
-
-  fprintf(pxd.fptr, "cdef extern from \"%s.h\":\n", libmodeHeadername);
-
-  for_vector(FnSymbol, fn, functions) {
-    if (isUserRoutine(fn)) {
-      fn->codegenPython(C_PXD);
-    }
-  }
+  writeFileContents(file);
 
   gGenInfo->cfile = save_cfile;
 
@@ -32,174 +21,160 @@ static void makePXDFile(std::vector<FnSymbol*> functions) {
   closeLibraryHelperFile(&pxd, false);
 }
 
+// Generate the .pxd file for the library.  This will be used when creating
+// the Python module
+static void makePXDFile(std::vector<FnSymbol*> functions) {
+  makePythonFile(pxdName.c_str(), "pxd", [&](fileinfo& pxd) {
+    fprintf(pxd.fptr, "from libc.stdint cimport *\n");
+    // Get the permanent runtime definitions
+    fprintf(pxd.fptr, "from chplrt cimport *\n\n");
+
+    fprintf(pxd.fptr, "cdef extern from \"%s.h\":\n", libmodeHeadername);
+
+    for_vector(FnSymbol, fn, functions) {
+      if (isUserRoutine(fn)) {
+        fn->codegenPython(C_PXD);
+      }
+    }
+  });
+}
+
 // Generate the .pyx file for the library.  This will also be used when
 // creating the Python module.
 static void makePYXFile(std::vector<FnSymbol*> functions) {
-  fileinfo pyx = { NULL, NULL, NULL };
+  makePythonFile(pythonModulename, "pyx", [&](fileinfo& pyx) {
+    fprintf(pyx.fptr, "#!python\n");
+    fprintf(pyx.fptr, "#cython: language_level=3\n");
+    // Make import statement at top of .pyx file for chpl_library_init and
+    // chpl_library_finalize
+    fprintf(pyx.fptr, "from chplrt cimport chpl_library_init, ");
+    fprintf(pyx.fptr, "chpl_library_finalize, chpl_external_array, ");
+    fprintf(pyx.fptr, "chpl_make_external_array, chpl_make_external_array_ptr");
+    fprintf(pyx.fptr, ", chpl_free_external_array, chpl_opaque_array, ");
+    fprintf(pyx.fptr, "cleanupOpaqueArray, chpl_free, ");
+    fprintf(pyx.fptr, "chpl_byte_buffer, chpl_byte_buffer_free, ");
+    fprintf(pyx.fptr, "PyBytes_FromStringAndSize\n");
 
-  openLibraryHelperFile(&pyx, pythonModulename, "pyx");
+    std::vector<FnSymbol*> moduleInits;
+    std::vector<FnSymbol*> exported;
 
-  if (pyx.fptr == NULL) {
-    closeLibraryHelperFile(&pyx, false);
-    return;
-  }
+    fprintf(pyx.fptr, "from %s cimport ", pxdName.c_str());
+    bool first = true;
+    // Make import statement at top of .pyx file for exported functions
+    for_vector(FnSymbol, fn, functions) {
+      if (!isUserRoutine(fn) || !fn->hasFlag(FLAG_EXPORT)) {
+        continue;
+      }
 
-  FILE* save_cfile = gGenInfo->cfile;
+      if (first) {
+        first = false;
+      } else {
+        fprintf(pyx.fptr, ", ");
+      }
 
-  gGenInfo->cfile = pyx.fptr;
-
-  fprintf(pyx.fptr, "#!python\n");
-  fprintf(pyx.fptr, "#cython: language_level=3\n");
-  // Make import statement at top of .pyx file for chpl_library_init and
-  // chpl_library_finalize
-  fprintf(pyx.fptr, "from chplrt cimport chpl_library_init, ");
-  fprintf(pyx.fptr, "chpl_library_finalize, chpl_external_array, ");
-  fprintf(pyx.fptr, "chpl_make_external_array, chpl_make_external_array_ptr");
-  fprintf(pyx.fptr, ", chpl_free_external_array, chpl_opaque_array, ");
-  fprintf(pyx.fptr, "cleanupOpaqueArray, chpl_free, ");
-  fprintf(pyx.fptr, "chpl_byte_buffer, chpl_byte_buffer_free, ");
-  fprintf(pyx.fptr, "PyBytes_FromStringAndSize\n");
-
-  std::vector<FnSymbol*> moduleInits;
-  std::vector<FnSymbol*> exported;
-
-  fprintf(pyx.fptr, "from %s cimport ", pxdName.c_str());
-  bool first = true;
-  // Make import statement at top of .pyx file for exported functions
-  for_vector(FnSymbol, fn, functions) {
-    if (!isUserRoutine(fn) || !fn->hasFlag(FLAG_EXPORT)) {
-      continue;
+      // Module initialization functions get handled together, other
+      // exported functions will have their own definition.
+      if (fn->hasFlag(FLAG_MODULE_INIT)) {
+        // No need to rename the module init function, the user won't see it
+        fprintf(pyx.fptr, "%s", fn->cname);
+        moduleInits.push_back(fn);
+      } else {
+        // On import, rename the exported function so that we can use its
+        // original name in the Python module to avoid confusion.
+        fprintf(pyx.fptr, "%s as chpl_%s", fn->cname, fn->cname);
+        exported.push_back(fn);
+      }
     }
+    fprintf(pyx.fptr, "\n\n");
 
-    if (first) {
-      first = false;
-    } else {
-      fprintf(pyx.fptr, ", ");
+    // Necessary for using numpy types
+    fprintf(pyx.fptr, "import numpy\n");
+    fprintf(pyx.fptr, "cimport numpy\n");
+    // Necessary for supporting pointers
+    fprintf(pyx.fptr, "import ctypes\n");
+    fprintf(pyx.fptr, "from libc.stdint cimport intptr_t\n\n");
+
+    makePYXSetupFunctions(moduleInits);
+    makeOpaqueArrayClass();
+
+    // Add Python wrapper for the exported functions, to translate the types
+    // appropriately
+    for_vector(FnSymbol, fn, exported) {
+      fn->codegenPython(PYTHON_PYX);
     }
-
-    // Module initialization functions get handled together, other
-    // exported functions will have their own definition.
-    if (fn->hasFlag(FLAG_MODULE_INIT)) {
-      // No need to rename the module init function, the user won't see it
-      fprintf(pyx.fptr, "%s", fn->cname);
-      moduleInits.push_back(fn);
-    } else {
-      // On import, rename the exported function so that we can use its
-      // original name in the Python module to avoid confusion.
-      fprintf(pyx.fptr, "%s as chpl_%s", fn->cname, fn->cname);
-      exported.push_back(fn);
-    }
-  }
-  fprintf(pyx.fptr, "\n\n");
-
-  // Necessary for using numpy types
-  fprintf(pyx.fptr, "import numpy\n");
-  fprintf(pyx.fptr, "cimport numpy\n");
-  // Necessary for supporting pointers
-  fprintf(pyx.fptr, "import ctypes\n");
-  fprintf(pyx.fptr, "from libc.stdint cimport intptr_t\n\n");
-
-  makePYXSetupFunctions(moduleInits);
-  makeOpaqueArrayClass();
-
-  // Add Python wrapper for the exported functions, to translate the types
-  // appropriately
-  for_vector(FnSymbol, fn, exported) {
-    fn->codegenPython(PYTHON_PYX);
-  }
-
-  gGenInfo->cfile = save_cfile;
-
-  // Don't "beautify", it will remove the tabs
-  closeLibraryHelperFile(&pyx, false);
+  })
 }
 
 // create the Python file which will be used to compile the .pyx, .pxd, library,
 // and header files into a Python module.
 static void makePYFile() {
-  fileinfo py = { NULL, NULL, NULL };
-
-  openLibraryHelperFile(&py, pythonModulename, "py");
-
-  if (py.fptr == NULL) {
-    closeLibraryHelperFile(&py, false);
-    return;
-  }
-
-  FILE* save_cfile = gGenInfo->cfile;
-
-  gGenInfo->cfile = py.fptr;
-
-  std::string libname = "";
-  int libLength = strlen("lib");
-  bool startsWithLib = strncmp(executableFilename, "lib", libLength) == 0;
-  if (startsWithLib) {
-    libname += &executableFilename[libLength];
-  } else {
-    libname = executableFilename;
-  }
-
-  // Imports
-  fprintf(py.fptr, "from setuptools import setup\n");
-  fprintf(py.fptr, "from setuptools import Extension\n");
-  fprintf(py.fptr, "from Cython.Build import cythonize\n");
-  fprintf(py.fptr, "import numpy\n\n");
-
-  // Get the static Chapel runtime and third-party libraries
-  fprintf(py.fptr, "chpl_libraries=[");
-  bool first = true;
-  // Get the libraries listed in require statements
-  for_vector(const char, libName, libFiles) {
-    if (first) {
-      first = false;
+  writePythonFile(pythonModulename, "py", [](fileinfo& py) {
+    std::string libname = "";
+    int libLength = strlen("lib");
+    bool startsWithLib = strncmp(executableFilename, "lib", libLength) == 0;
+    if (startsWithLib) {
+      libname += &executableFilename[libLength];
     } else {
-      fprintf(py.fptr, ", ");
+      libname = executableFilename;
     }
-    fprintf(py.fptr, "\"%s\"", libName);
-  }
-  std::string libraries = getCompilelineOption("libraries");
 
-  // Erase trailing newline and append multilocale-only dependencies.
-  if (fMultiLocaleInterop) {
-    libraries.erase(libraries.length() - 1);
-    libraries += " ";
-    libraries += getCompilelineOption("multilocale-lib-deps");
-  }
+    // Imports
+    fprintf(py.fptr, "from setuptools import setup\n");
+    fprintf(py.fptr, "from setuptools import Extension\n");
+    fprintf(py.fptr, "from Cython.Build import cythonize\n");
+    fprintf(py.fptr, "import numpy\n\n");
 
-  char copyOfLib[libraries.length() + 1];
-  libraries.copy(copyOfLib, libraries.length(), 0);
-  copyOfLib[libraries.length()] = '\0';
-  int prefixLen = strlen("-l");
-  char* curSection = strtok(copyOfLib, " \n");
-  // Get the libraries from compileline --libraries, taking the `name`
-  // portion from all `-lname` parts of that command's output
-  while (curSection != NULL) {
-    if (strncmp(curSection, "-l", prefixLen) == 0) {
+    // Get the static Chapel runtime and third-party libraries
+    fprintf(py.fptr, "chpl_libraries=[");
+    bool first = true;
+    // Get the libraries listed in require statements
+    for_vector(const char, libName, libFiles) {
       if (first) {
         first = false;
       } else {
         fprintf(py.fptr, ", ");
       }
-      fprintf(py.fptr, "\"%s\"", &curSection[prefixLen]);
+      fprintf(py.fptr, "\"%s\"", libName);
     }
-    curSection = strtok(NULL, " \n");
-  }
+    std::string libraries = getCompilelineOption("libraries");
 
-  // Fetch addition
-  fprintf(py.fptr, "]\n");
+    // Erase trailing newline and append multilocale-only dependencies.
+    if (fMultiLocaleInterop) {
+      libraries.erase(libraries.length() - 1);
+      libraries += " ";
+      libraries += getCompilelineOption("multilocale-lib-deps");
+    }
 
-  // Cythonize me, Captain!
-  fprintf(py.fptr, "setup(name = '%s library',\n", pythonModulename);
-  fprintf(py.fptr, "\text_modules = cythonize(\n");
-  fprintf(py.fptr, "\t\tExtension(\"%s\",\n", pythonModulename);
-  fprintf(py.fptr, "\t\t\tinclude_dirs=[numpy.get_include()],\n");
-  fprintf(py.fptr, "\t\t\tsources=[\"%s.pyx\"],\n", pythonModulename);
-  fprintf(py.fptr, "\t\t\tlibraries=[\"%s\"] + chpl_libraries + "
-                    "[\"%s\"])))\n",
-                    libname.c_str(), libname.c_str());
+    char copyOfLib[libraries.length() + 1];
+    libraries.copy(copyOfLib, libraries.length(), 0);
+    copyOfLib[libraries.length()] = '\0';
+    int prefixLen = strlen("-l");
+    char* curSection = strtok(copyOfLib, " \n");
+    // Get the libraries from compileline --libraries, taking the `name`
+    // portion from all `-lname` parts of that command's output
+    while (curSection != NULL) {
+      if (strncmp(curSection, "-l", prefixLen) == 0) {
+        if (first) {
+          first = false;
+        } else {
+          fprintf(py.fptr, ", ");
+        }
+        fprintf(py.fptr, "\"%s\"", &curSection[prefixLen]);
+      }
+      curSection = strtok(NULL, " \n");
+    }
 
-  gGenInfo->cfile = save_cfile;
+    // Fetch addition
+    fprintf(py.fptr, "]\n");
 
-  // Don't "beautify", it will remove the tabs
-  closeLibraryHelperFile(&py, false);
+    // Cythonize me, Captain!
+    fprintf(py.fptr, "setup(name = '%s library',\n", pythonModulename);
+    fprintf(py.fptr, "\text_modules = cythonize(\n");
+    fprintf(py.fptr, "\t\tExtension(\"%s\",\n", pythonModulename);
+    fprintf(py.fptr, "\t\t\tinclude_dirs=[numpy.get_include()],\n");
+    fprintf(py.fptr, "\t\t\tsources=[\"%s.pyx\"],\n", pythonModulename);
+    fprintf(py.fptr, "\t\t\tlibraries=[\"%s\"] + chpl_libraries + "
+                      "[\"%s\"])))\n",
+                      libname.c_str(), libname.c_str());
+  })
 }
